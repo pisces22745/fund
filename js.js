c3b63d88564ec2d869bee70693c071eb24a04be4
shaoluqin <578992047>
2017-11-30 12:54:17 +0800

    异步ajax请求文件

--- js.js
+++ js.js
@@ -152,7 +152,9 @@ var fund = {
         return +str
     },
     arrayDuplicate: function (array) {
-        return [...new Set(array)]
+        return [...new
+        Set(array)
+        ]
     },
     getMax: function (numbers) {    //取出一个数组中的最大值
         return Math.max.apply(Math, numbers);
@@ -175,7 +177,8 @@ var fund = {
         })()
     },
     setStore: (name, content) => {  // localstorage操作
-        if (!name) return
+        if (!name)
+            return
         if (typeof content !== 'string') {
             content = JSON.stringify(content)
         }
@@ -188,5 +191,32 @@ var fund = {
     removeStore: name => {
         if (!name) return
         window.localStorage.removeItem(name)
+    },
+    downloadExcel: () => {
+        var url = 'test1.xlsx'; // 请求地址
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true); // 也可以使用POST方式，根据接口
+        xhr.responseType = "blob"; // 返回类型blob
+        // 定义请求完成的处理函数，请求前也可以增加加载框/禁用下载按钮逻辑
+        xhr.onload = function() {
+            // 请求完成
+            if(this.status === 200) {
+                // 返回200
+                var blob = this.response;
+                var reader = new FileReader();
+                reader.readAsDataURL(blob); // 转换为base64，可以直接放入a表情href
+                reader.onload = function(e) {
+                    // 转换完成，创建一个a标签用于下载
+                    var a = document.createElement('a');
+                    a.download = 'data.xlsx';
+                    a.href = e.target.result;
+                    $("body").append(a); // 修复firefox中无法触发click
+                    a.click();
+                    $(a).remove();
+                }
+            }
+        };
+        // 发送ajax请求
+        xhr.send()
     }
 }
